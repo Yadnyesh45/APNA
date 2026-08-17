ad3034def98854564ee67cac60af6401c8333f73
yadnyesh <yadnyeshpawar34gmail.com>
2026-08-17 19:06:17 +0530

    add index style script

---
+++ script.js
@@ -0,0 +1,4 @@
+let buttton = document.getElementById("btn")
+buttton.addEventListener("click", () => {
+    document.querySelector(".para").innerHTML = "A guy who can able to create web pages currently learning react.js";
+})
