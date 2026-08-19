873428d79860f14ae7e2ad7d0eb4d9c0c4206c8a
Yadnyesh45 <yadnyeshpawar34@gmail.com>
2026-08-19 18:53:48 +0530

    initial committ
    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let buttton = document.getElementById("btn")
-buttton.addEventListener("click", () => {
-    document.querySelector(".para").innerHTML = "A guy who can able to create web pages currently learning react.js";
-})
+// let buttton = document.getElementById("btn")
+// buttton.addEventListener("click", () => {
+//     document.querySelector(".para").innerHTML = "A guy who can able to create web pages currently learning react.js";
+// )
+// let About = document.querySelector(".btn2");
+// let content = document.getElementById("#innerContent")
+
+// About.addEventListener("click",() =>{
+//     alert("hi")
+// content.classList.toggle('is-open');
+// });
