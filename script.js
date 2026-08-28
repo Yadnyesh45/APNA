231c0c37a752f6bbc1b2eb7f0fd9981aaf482733
Yadnyesh45 <yadnyeshpawar34@gmail.com>
2026-08-28 16:59:07 +0530

    update index,script,style

--- script.js
+++ script.js
@@ -9,3 +9,31 @@
 //     alert("hi")
 // content.classList.toggle('is-open');
 // });
+
+// Grab the buttons
+const homeBtn = document.getElementById('homeBtn');
+const aboutBtn = document.getElementById('aboutBtn');
+const skillsBtn = document.getElementById('skillsBtn');
+const contactBtn = document.getElementById('contactBtn');
+
+// Grab the sections
+const homeSection = document.getElementById('home');
+const aboutSection = document.getElementById('about');
+const skillsSection = document.getElementById('skills');
+const contactSection = document.getElementById('contact');
+
+homeBtn.addEventListener('click', function () {
+    homeSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+aboutBtn.addEventListener('click', function () {
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+skillsBtn.addEventListener('click', function () {
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+contactBtn.addEventListener('click', function () {
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+});
